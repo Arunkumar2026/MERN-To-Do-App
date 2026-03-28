d1259f327e96a4b1a8af01c09dbd487368b143c2
Arun Kumar <akulakumar453@gmail.com>
2026-03-28 17:10:58 +0530

    This is API Routes file

---
+++ backend/routes/todo.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { createTodo } from "../controllers/todo.controller";
+
+const router = express.Router();
+
+router.post("/", createTodo);
+
+export default router;
