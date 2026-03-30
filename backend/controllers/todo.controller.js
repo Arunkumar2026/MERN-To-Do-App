f92f0bf934b77b7f48b5158282539c233e0b7fa6
Arun Kumar <akulakumar453@gmail.com>
2026-03-30 19:35:41 +0530

    This is API Methods file

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -19,3 +19,50 @@ export const createTodo = async (req, res) => {
         res.status(500).json({message: error.message});
     }
 };
+
+export const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find();
+
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({message: error.message });
+    }
+};
+
+export const updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const updateTodo = await Todo.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true}
+
+        );
+        
+        if(!updateTodo){
+            return res.status(404).json({message: "Todo not found" });
+        }
+
+        res.status(200).json({updateTodo});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deleteTodo = await Todo.findByIdAndDelete(id);
+
+        if(!deleteTodo){
+            res.status(404).json({message: "Todo not found" });
+        }
+
+        res.status(200).json({message: "Todo deleted successfully" });
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
