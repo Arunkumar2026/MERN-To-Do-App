71f252b76c55b0976eb4d00863e4c685fe7bea32
Arun Kumar <akulakumar453@gmail.com>
2026-04-16 23:22:33 +0530

    this is backend todo routes file

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-
 import { createTodo, getTodos, updateTodo, deleteTodo } from "../controllers/todo.controller.js";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/", createTodo);
+router.use(protect);
+
 router.get("/", getTodos);
+router.post("/", createTodo);
 router.put("/:id", updateTodo);
 router.delete("/:id", deleteTodo);
 
